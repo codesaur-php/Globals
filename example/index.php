2aca375f263cc00636ef563f08015d1d181ebf20
Наранхүү <codesaur@gmail.com>
2021-02-27 16:43:52 +0800

    Update index.php

--- example/index.php
+++ example/index.php
@@ -11,11 +11,9 @@ use codesaur\Globals\Post;
 use codesaur\Globals\Server;
 
 $post = new Post();
-
 if ($post->has('name')) {
     echo "POST['name']: " . $post->value('name') . '<br />';
 }
 
 $server = new Server();
-
 echo 'Your IP: ' . $server->getRemoteAddr();
