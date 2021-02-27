b0340217c84a34b96fa0817bdef3767660f127ae
Наранхүү <codesaur@gmail.com>
2021-02-27 16:39:12 +0800

    example script created

---
+++ example/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/* DEV: v1.2021.02.27
+ * 
+ * This is an example script!
+ */
+
+require_once '../vendor/autoload.php';
+
+use codesaur\Globals\Post;
+use codesaur\Globals\Server;
+
+$post = new Post();
+
+if ($post->has('name')) {
+    echo "POST['name']: " . $post->value('name') . '<br />';
+}
+
+$server = new Server();
+
+echo 'Your IP: ' . $server->getRemoteAddr();
